0aeb2224c84cb085bbb69b11d3396efd7a4b1031
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2024-01-12 22:08:07 +0530

    Addition of xenial support
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- ros-xenial/install.sh
+++ ros-xenial/install.sh
@@ -4,3 +4,5 @@
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | gpg --dearmor - | sudo gpg --dearmor -o /usr/share/keyrings/ros-archive-keyring.gpg
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list';
 sudo apt install -y ros-base
+
+sudo sh -c '. /etc/lsb-release && echo "deb http://mirror-ap.packages.ros.org/ros/ubuntu/ noetic main" > /etc/apt/sources.list.d/ros-latest.list'
